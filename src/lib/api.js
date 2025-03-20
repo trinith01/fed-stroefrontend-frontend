6fb587bbc76ec22a1f221997a2049701bc18b5c0
trinith01 <gt202054194@gmail.com>
2025-03-21 01:34:16 +0530

    edot base url

--- src/lib/api.js
+++ src/lib/api.js
@@ -10,7 +10,7 @@ export const Api = createApi({
   reducerPath: "Api",
   // baseQuery: fetchBaseQuery({ baseUrl: baseUrl }),
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:8000/api/",
+    baseUrl: "https://fed-storefrontend-backend-trinith.onrender.com/api/",
     prepareHeaders: async (headers, { getState }) => {
       const token = await window.Clerk?.session?.getToken();
       if (token) {
